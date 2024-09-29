e531cf5a8a4d9b86aeb4944275047bf5a579a973
sarahvue <sarahvue0924@gmail.com>
2024-09-29 09:55:04 -0700

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <footer>
+        <p>This project was coded by Sarah and is open-sourced.</p>
+        <a
+          href="https://github.com/sarahvue/react-weather-application"
+          target="_blank"
+        >
+          View Repo Here
+        </a>
+      </footer>
+    </div>
+  );
 }
